2f7087b9df5c33600001ccd26e87578b3b310f56
stephen-shichen <jsczj.canada@gmail.com>
2023-01-31 05:34:38 -0500

    Backend First Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,103 @@
 from flask import Flask
+import base64
+import random
 
-global memcache
+global memcache_global, dummyDB
+
+class memcache_structure:
+    def __init__(self):
+        self.memcache = {}
+        self.memcache_size = 10*1024*1024
+        self.memcache_mode = "RR"
+        self.current_num_items = 0
+        self.current_size = 0
+        self.num_requests = 0
+        self.miss = 0
+        self.hit = 0
+        self.access_tracker = None
+        
+    def memcache_invalidate(self, key):
+        if key in self.memcache:
+            self.current_size = self.current_size - len(self.memcache[key])
+            self.current_num_items = self.current_num_items - 1
+            del self.memcache[key]
+            if self.memcache_mode == "LRU":
+                self.access_tracker.remove(key)
+            return "OK"
+        else:
+            return "Unknown key"
+    
+    def memcache_evict(self):
+        if self.memcache_mode == "RR":
+            key_evict = random.choice(list(self.memcache.keys()))
+            self.memcache_invalidate(key_evict)
+        elif self.memcache_mode == "LRU":
+            key_evict = self.access_tracker[0]
+            self.memcache_invalidate(key_evict)
+
+    def memcache_reconfigure(self, size, mode):
+        self.num_requests = self.num_requests + 1
+        while size < self.memcache_size:
+            self.memcache_evict()
+        self.memcache_size = size
+        if self.memcache_mode != mode: 
+            self.memcache_mode = mode
+            if self.memcache_mode == "RR":
+                self.access_tracker = None
+            elif self.memcache_mode == "LRU":
+                self.access_tracker = []
+
+    def memcache_put(self, key, value): 
+        self.num_requests = self.num_requests + 1
+        if key in self.memcache:
+            self.memcache_invalidate(key)
+        while self.current_size + len(value) > self.memcache_size:
+            self.memcache_evict()
+        self.memcache[key] = value
+        self.current_size = self.current_size + len(value)
+        self.current_num_items = self.current_num_items + 1
+        if self.memcache_mode == "LRU": 
+            self.access_tracker.append(key)
+
+    def memcache_clear(self):
+        self.num_requests = self.num_requests + 1
+        self.memcache.clear()
+        self.current_num_items = 0
+        self.current_size = 0
+        if self.memcache_mode == "LRU": 
+            self.access_tracker = []
+
+    def memcache_get(self, key):
+        self.num_requests = self.num_requests + 1
+        if key in self.memcache:
+            self.hit = self.hit + 1
+            if self.memcache_mode == "LRU": 
+                self.access_tracker.remove(key)
+                self.access_tracker.append(key)
+            return self.memcache[key]
+        else:
+            self.miss = self.miss + 1
+            return None
+
+    def memcache_allkeys(self):
+        self.num_requests = self.num_requests + 1
+        return list(self.memcache.keys())
+    
+    def current_configuration(self):
+        full_list = []
+        full_list.append(self.memcache_size)
+        full_list.append(self.memcache_mode)
+        full_list.append(self.current_num_items)
+        full_list.append(self.current_size)
+        full_list.append(self.num_requests)
+        full_list.append(self.miss)
+        full_list.append(self.hit)
+        return full_list
 
 webapp = Flask(__name__)
-memcache = {}
+memcache_global = memcache_structure();
+dummyDB = {}
+
 
 from app import main
 
--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-
 from flask import render_template, url_for, request
-from app import webapp, memcache
+from app import webapp, memcache_structure, memcache_global, dummyDB
 from flask import json
+from pathlib import Path
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+import time
+import atexit
 
+os_file_path = "C:/Users/jscst/Flask_Test/"
+
+def print_date_time():
+    #We will use this function to implement the timing storage
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=5)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 @webapp.route('/')
 def main():
@@ -11,11 +28,54 @@ def main():
 @webapp.route('/get',methods=['POST'])
 def get():
     key = request.form.get('key')
-
-    if key in memcache:
-        value = memcache[key]
+    value = memcache_global.memcache_get(key)
+    
+    if value is None:
+        response = webapp.response_class(
+            response=json.dumps("Unknown key"),
+            status=400,
+            mimetype='application/json'
+        )
+    else:
         response = webapp.response_class(
             response=json.dumps(value),
+            status=200,
+            mimetype='application/json'
+        )
+    return response
+
+
+@webapp.route('/put',methods=['POST'])
+def put():
+    key = request.form.get('key')
+    value = request.form.get('value')
+    memcache_global.memcache_put(key, value)
+
+    response = webapp.response_class(
+        response=json.dumps("OK"),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@webapp.route('/clear',methods=['POST'])
+def clear():
+    memcache_global.memcache_clear()
+
+    response = webapp.response_class(
+        response=json.dumps("OK"),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@webapp.route('/invalidateKey',methods=['POST'])
+def invalidateKey():
+    key = request.form.get('key')
+    message = memcache_global.memcache_invalidate(key)
+    if message == "OK":
+        response = webapp.response_class(
+            response=json.dumps("OK"),
             status=200,
             mimetype='application/json'
         )
@@ -25,14 +85,38 @@ def get():
             status=400,
             mimetype='application/json'
         )
-
     return response
 
-@webapp.route('/put',methods=['POST'])
-def put():
+@webapp.route('/refreshConfiguration',methods=['POST'])
+def refreshConfiguration():
+    # Pending DB code
+    size = 50*1024*1024
+    mode = "RR"
+    # End Pending DB code
+    memcache_global.memcache_reconfigure(size, mode)
+    response = webapp.response_class(
+        response=json.dumps("OK"),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+#def schedulingUpdateStat(): 
+
+
+# Pass from Front End Functions
+@webapp.route('/uploadToDB',methods=['POST'])
+def uploadToDB():
     key = request.form.get('key')
     value = request.form.get('value')
-    memcache[key] = value
+    #Omit DB code
+    dummyDB[key] = "yes"
+    #End of using dummyDB
+    full_file_path = os.path.join(os_file_path, key)
+    if os.path.isfile(full_file_path):
+        os.remove(full_file_path)
+    with open(full_file_path, 'w') as fp:
+        fp.write(value)
 
     response = webapp.response_class(
         response=json.dumps("OK"),
@@ -41,4 +125,99 @@ def put():
     )
 
     return response
+    
+@webapp.route('/getFromDB',methods=['POST'])
+def getFromDB():
+    key = request.form.get('key')
+    full_file_path = os.path.join(os_file_path, key)
+    if not os.path.isfile(full_file_path):
+        response = webapp.response_class(
+            response=json.dumps("File Not Found"),
+            status=400,
+            mimetype='application/json'
+        )
+    else: 
+        value=Path(full_file_path).read_text()
+        response = webapp.response_class(
+            response=json.dumps(value),
+            status=200,
+            mimetype='application/json'
+        )
 
+    return response
+
+@webapp.route('/allKeyDB',methods=['POST'])
+def allKeyDB():
+    #Omit DB code
+    allKeys = list(dummyDB.keys())
+    #End of using dummyDB
+
+    response = webapp.response_class(
+        response=json.dumps(allKeys),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+@webapp.route('/deleteAllFromDB',methods=['POST'])
+def deleteAllFromDB():
+    #Omit DB code
+    allKeys = list(dummyDB.keys())
+    #End of using dummyDB
+    for key in allKeys: 
+        full_file_path = os.path.join(os_file_path, key)
+        if os.path.isfile(full_file_path):
+            os.remove(full_file_path)
+    response = webapp.response_class(
+        response=json.dumps("OK"),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+@webapp.route('/allKeyMemcache',methods=['GET'])
+def allKeyMemcache():
+    allKeys = memcache_global.memcache_allkeys()
+    response = webapp.response_class(
+        response=json.dumps(allKeys),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@webapp.route('/configureMemcache',methods=['POST'])
+def configureMemcache():
+    size = request.form.get('size')
+    mode = request.form.get('mode')
+    #omit DB code
+    response = webapp.response_class(
+        response=json.dumps("OK"),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@webapp.route('/requestCurrentStat',methods=['GET'])
+def requestCurrentStat():
+    #Omit DB code
+    allKeys = list(dummyDB.keys())
+    #End of using dummyDB
+    response = webapp.response_class(
+        response=json.dumps(allKeys),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+#For debugging purpose
+@webapp.route('/currentConfig',methods=['GET'])
+def currentConfig():
+    configurationList = memcache_global.current_configuration()
+    response = webapp.response_class(
+        response=json.dumps(configurationList),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
